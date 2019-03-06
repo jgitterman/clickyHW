d7b7afba4ef9cceae8f11a0a6f39d113c98e9566
Justin Gitterman <justingitterman@gmail.com>
2019-03-06 13:38:01 -0500

    Updates

---
+++ precache-manifest.533665f36f709edd2ed3d449bdb53ebd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfd6a56cbe8fa01f29d1",
+    "url": "/clickyHW/static/js/runtime~main.dfd6a56c.js"
+  },
+  {
+    "revision": "052569e542cac270daf2",
+    "url": "/clickyHW/static/js/main.052569e5.chunk.js"
+  },
+  {
+    "revision": "179a064784d3fb038987",
+    "url": "/clickyHW/static/js/2.179a0647.chunk.js"
+  },
+  {
+    "revision": "e0f89e302c005105f23a9c0c5f657c37",
+    "url": "/clickyHW/index.html"
+  }
+];
